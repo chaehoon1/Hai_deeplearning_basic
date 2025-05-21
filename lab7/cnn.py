db53a327637df4c3828499bb83db8c5dd901be06
chaehoon1 <yurim7p@gmail.com>
2025-05-22 08:56:37 +0900

    m

--- lab7/cnn.py
+++ lab7/cnn.py
@@ -6,4 +6,9 @@ con_layer = nn.Conv2d(3, 8, (3, 5), stride = (2, 1), padding = (4, 2), dilation 
 input = torch.randn(20, 3, 28, 28)
 output = con_layer(input)
 
+con_layer2 = nn.Conv2d(3, 8, 5, padding = "same")
+
+output2 = con_layer2(input)
+
 print(output)
+print(output2)
